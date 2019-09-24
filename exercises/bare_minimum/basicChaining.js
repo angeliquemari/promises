c6fec2467c4ac2760e25d220c88701a3e71cd092
angeliquemari <angelique@nehmzow.com>
2019-09-24 16:56:15 -0700

    add basicChaining function

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,13 +8,42 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require('fs');
 var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var request = require('request');
 
+var getGitHubProfile = function(user) {
+  var options = {
+    url: 'https://api.github.com/users/' + user,
+    headers: { 'User-Agent': 'request' },
+    json: true // will JSON.parse(body) for us
+  };
 
+  return new Promise(function(resolve, reject) {
+    request.get(options, function(err, data, body) {
+      if (err) { return reject(err); }
+
+      var simpleProfile = {
+        id: body.id,
+        login: body.login,
+        name: body.name,
+        company: body.company,
+        location: body.location
+      };
+      resolve(simpleProfile);
+    });
+  });
+};
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return fs.readFileAsync(readFilePath)
+    .then(function (fileData) {
+      var username = fileData.toString().slice(0, fileData.toString().indexOf('\n'));
+      return getGitHubProfile(username);
+    })
+    .then(function (userProfile) {
+      return fs.writeFileAsync(writeFilePath, JSON.stringify(userProfile));
+    });
 };
 
 // Export these functions so we can test them
